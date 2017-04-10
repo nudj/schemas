5a10ddebc83440961565410f445fd5abc2cca91f
Nick Collings <nick@collingo.com>
2017-04-10 10:06:47 +0100

    Add applications schema

---
+++ src/lib/schemas/applications.js
@@ -0,0 +1,20 @@
+module.exports = {
+  jobId: {
+    example: {
+      fn: 'choice',
+      args: [['1']]
+    }
+  },
+  personId: {
+    example: {
+      fn: 'choice',
+      args: [['1']]
+    }
+  },
+  referralId: {
+    example: {
+      fn: 'choice',
+      args: [['1']]
+    }
+  }
+}
